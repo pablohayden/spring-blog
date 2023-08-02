8e63ec51cb069a9e6d7512aa3fdffbe4ba554045
pablohayden <paulie.hayden@gmail.com>
2023-08-02 14:29:19 +0100

    initial

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,19 +9,19 @@ create table IF NOT EXISTS Author(
 
 -- post
 create table IF NOT EXISTS Post (
-    id serial primary key ,
+    id serial primary key,
     version int,
     title varchar(255) not null,
     content text not null,
     published_on timestamp not null,
     updated_on timestamp,
-    author int,
+    author BIGINT UNSIGNED,
     foreign key (author) references Author(id)
 );
 
 -- comment
 create table IF NOT EXISTS Comment(
-    post int not null,
+    post BIGINT UNSIGNED not null,
     name varchar(100) not null,
     content text not null,
     published_on timestamp not null,
